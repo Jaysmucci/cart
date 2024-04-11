7c255b3534aac556ea0db8dc7482f4f700ac1d33
Jays Mucci <jaysmuchi@gmail.com>
2024-04-11 14:06:24 +0100

    This is a shopping cart project

---
+++ index.js
@@ -0,0 +1,7 @@
+// Author - Jaysmucci
+// Project version - 0.1.1
+// Project Description - This is a simple shopping cart App that enables users buy products 
+// online anywhere they are across Africa
+
+
+// Project still under Development
